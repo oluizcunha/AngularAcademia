874d4d637b869802d9f1a34e36aabd7b104fe7f2
Luiz <luizotcunha@gmail.com>
2021-06-30 08:10:08 -0400

    hooks e componente filho

--- angular-first/src/app/componente-filho/componente-filho.component.ts
+++ angular-first/src/app/componente-filho/componente-filho.component.ts
@@ -1,15 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Input,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-componente-filho',
   templateUrl: './componente-filho.component.html',
-  styleUrls: ['./componente-filho.component.css']
+  styleUrls: ['./componente-filho.component.css'],
 })
-export class ComponenteFilhoComponent implements OnInit {
+export class ComponenteFilhoComponent
+  implements
+    OnChanges,
+    OnInit,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
+  @Input() mensagem = ' ';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    console.log('ChildComponent:Constructor');
   }
 
+  ngOnChanges() {
+    console.log('ChildComponent:OnChanges');
+  }
+
+  ngOnInit() {
+    console.log('ChildComponent:OnInit');
+  }
+
+  ngDoCheck() {
+    console.log('ChildComponent:DoCheck');
+  }
+
+  ngAfterContentInit() {
+    console.log('ChildComponent:AfterContentInit');
+  }
+
+  ngAfterContentChecked() {
+    console.log('ChildComponent:AfterContentChecked');
+  }
+
+  ngAfterViewInit() {
+    console.log('ChildComponent:AfterViewInit');
+  }
+
+  ngAfterViewChecked() {
+    console.log('ChildComponent:AfterViewChecked');
+  }
+
+  ngOnDestroy() {
+    console.log('ChildComponent:OnDestroy');
+  }
 }
